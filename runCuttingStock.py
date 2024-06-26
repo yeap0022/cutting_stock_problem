db57e9e3430eb2cd010d1359f1ee30740cbd5999
Yeap Yew Ming <eric_yeap_90@hotmail.com>
2024-06-26 16:33:05 +0800

    update runCuttingStock

--- runCuttingStock.py
+++ runCuttingStock.py
@@ -28,24 +28,6 @@ def main_execution(file_in: str = None, file_out: str = None):
     
     return dt_out
 
-def get_parser():
-    parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter,
-                                     description="Basic usage example: "
-                                                 "\"python main_test.py --_solver=CBC "
-                                                 "--file_in=[case1.json] --file_out=[case1.json] --file_in_2=[case1.json]\"")
-    parser.add_argument("--file_in", type=str, default=None, metavar="input_file_name",
-                        help="File name of the data. Default value: None. \n" +
-                            "If not specified, it will take data in json format from stdin")
-    parser.add_argument("--file_in_2", type=str, default=None, metavar="input_file_name",
-                        help="File name of the data. Default value: None. \n" +
-                            "If not specified, it will take data in json format from stdin")
-    parser.add_argument("--stdout", action="store_true", help="Output the result to stdout, default: False")
-    parser.add_argument("--file_out", type=str, default=None, metavar="output_file_name")
-    
-    parser.add_argument("--ide_test", action="store_true",
-                        help="TEMP: only for IDE testing purpose")
-    
-    return parser
 
 if __name__ == "__main__":
     input_file = "./input/order.xlsx"
